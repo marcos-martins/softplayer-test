e59a763a2ef79a1a3581cdf2d509aeaf639ec689
Marcos Aurélio <m.martins@outlook.com>
2019-02-11 22:32:19 +0000

    Criado o controler calculadora

---
+++ Softplayer.Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Softplayer.Calculadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [ActionName("calculajuros")]
+        public ActionResult<IEnumerable<string>> CalcularJuros()
+        {
+            return new string[] { "value1", "value2" };
+        }        
+    }
+}
