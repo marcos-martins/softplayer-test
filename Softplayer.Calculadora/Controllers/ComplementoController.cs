8e93c0157dad0f15864259b3f10b67d498dff396
Marcos Aurélio <m.martins@outlook.com>
2019-02-12 17:34:59 +0000

    Alterado o metodo show me the code para buscar o valor a partir de uma variavel de ambiente

--- Softplayer.Calculadora/Controllers/ComplementoController.cs
+++ Softplayer.Calculadora/Controllers/ComplementoController.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Softplayer.Calculadora.Controllers
 {
     [Route("v1/[controller]/[action]")]
     public class ComplementoController : ControllerBase
     {
+        private readonly IConfiguration _config;
+        public ComplementoController(IConfiguration config)
+        {
+            _config = config;
+        }
+       
+
         /// <summary>
         /// Retornar o caminho de onde se encontra o codigo
         /// </summary> 
@@ -12,7 +20,7 @@ namespace Softplayer.Calculadora.Controllers
         [HttpGet, ActionName("showmethecode")]
         public ActionResult<string> Get()
         {  
-            return "https://github.com/marcos-martins/softplayer-test"; 
+            return _config["SourceCode"]; 
         }    
     }
 }
