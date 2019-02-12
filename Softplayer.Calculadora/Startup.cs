1d89776009450458e7e92cd2918f425b255ed459
Marcos Aurélio <m.martins@outlook.com>
2019-02-12 17:18:48 +0000

    Ajustado para mostrar a pagina do swagger como pagina default

--- Softplayer.Calculadora/Startup.cs
+++ Softplayer.Calculadora/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -30,8 +31,14 @@ namespace Softplayer.Calculadora
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build();
+                });
+            });
             // Registering Swagger Generator
             services.AddSwaggerGen(c =>
             {
@@ -54,6 +61,8 @@ namespace Softplayer.Calculadora
             //Mock dataBase
             services.AddTransient<ICalculoRepositorio,CalculoRepositorio>();
             services.AddTransient<ICalculadoraServico,CalculadoraServico>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
         }
 
@@ -79,6 +88,11 @@ namespace Softplayer.Calculadora
                 // specifying the Swagger JSON endpoint.
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculadora");
             });
+
+            app.UseCors("CorsPolicy");
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
             
             app.UseMvc();
         }
